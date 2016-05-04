db008e94e745227985d564b10f6b374cc89ae257
Maks <maksdpua@gmail.com>
2016-05-05 00:05:37 +0300

    Struct fun

--- Classes Swift.playground/Contents.swift
+++ Classes Swift.playground/Contents.swift
@@ -23,11 +23,65 @@ class Student {
     }
 }
 
-let student1 = Student(_name: "Bob", _age: 18)
+struct StudentStruct {
+    
+    var name : String
+    var age: Int
+}
+
+let stClass1 = Student(_name: "Bob", _age: 18)
+
 
 
 //student1.name = "Alex"
 //student1.age = 21
-print(student1.name)
-print(student1.age)
+stClass1.name
+stClass1.age
+
+let stClass2 = stClass1
+
+stClass2.age = 11
+stClass2.name = "AAAA"
+
+stClass2.name
+stClass1.age
+
+stClass1.age
+stClass1.name
+
+var stStruct1 = StudentStruct(name: "A", age: 20)
+//stStruct.name = "A2"
+//stStruct.age = 25
+
+
+var stStruct2 = stStruct1
+
+stStruct2.name
+stStruct2.age
+
+stStruct2.name = "Jay"
+stStruct2.age = 23
+
+stStruct2.name
+stStruct2.age
+
+func addOneYear(student: Student) {
+    student.age += 1
+}
+
+func addOneYear(student: StudentStruct) {
+    student.age += 1
+}
+
+stStruct1.age
+addOneYear(stStruct1)
+stStruct1.age
+
+stClass1.age
+addOneYear(stClass1)
+stClass1.age
+
+
+
+
 
